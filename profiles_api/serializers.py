5e3e2993b8fa4872d59ec08c028317dd7caa9654
Andrey Kuskov <techinek@gmail.com>
2022-05-08 13:36:35 +0300

    Added UserProfile serializer

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,25 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+from .models import UserProfile
 
 
-class HiSeri(serializers.Serializer):
-    name = serializers.CharField(max_length=10)
+class UserProfileSerializer(ModelSerializer):
 
+    class Meta:
+        model = UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """Triggers custom create_user function of UserProfileManager"""
+        user = UserProfile.objects.create_user(
+                email=validated_data['email'],
+                name=validated_data['name'],
+                password=validated_data['password'])
+
+        return user
