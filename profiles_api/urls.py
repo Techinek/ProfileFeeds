b820c3788ce124ea229cad794b59073acf014fc2
Andrey Kuskov <techinek@gmail.com>
2022-05-08 13:03:28 +0300

    Checked Viewsets

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from .views import HiApiView
+from .views import HiApiView, HiViewSet
+
+router = DefaultRouter()
+router.register('viewset', HiViewSet, basename='viewset')
 
 urlpatterns = [
-    path('hi/', HiApiView.as_view(), name='hi')
+    path('apiview/', HiApiView.as_view(), name='api-view'),
+    path('', include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.viewsets import ViewSet
 
 from .serializers import HiSeri
 
@@ -38,5 +39,37 @@ class HiApiView(APIView):
         return Response({'method': 'DELETE'})
 
 
+class HiViewSet(ViewSet):
+    serializer_class = HiSeri
+
+    def list(self, request):
+        viewset_features = [
+            'HTTP represented as functions: list, create, retrieve',
+            'More functionality with less code',
+            'Mapped to URLS'
+        ]
+        return Response({'message': 'Hi Viewset',
+                         'features': viewset_features})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            return Response({'message': name})
+        else:
+            return Response(serializer.errors,
+                             status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        return Response({'method': 'Retrieved GET'})
+
+    def update(self, request, pk=None):
+        return Response({'method': 'Updated PUT'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'method': 'Partial PATCH'})
+
+    def destroy(self, request, pk=None):
+        return Response({'method': 'Destroyed DELETE'})
 
 
