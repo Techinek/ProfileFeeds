c5ae06128fde7ffe3c0238a6fc7332d4e9c5eccf
Andrey Kuskov <techinek@gmail.com>
2022-05-08 14:05:08 +0300

    Added permissions to CRUD only your own profile. Added token auth

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import TokenAuthentication
 
+from .permissions import UpdateOwnProfile
 from .models import UserProfile
 from .serializers import UserProfileSerializer
 
@@ -7,3 +9,5 @@ from .serializers import UserProfileSerializer
 class UserProfileViewSet(ModelViewSet):
     serializer_class = UserProfileSerializer
     queryset = UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (UpdateOwnProfile,)
