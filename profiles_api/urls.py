f3b63a413e572c3e434da82b5693d17fc2c5caf6
Andrey Kuskov <techinek@gmail.com>
2022-05-10 10:51:44 +0300

    Added login by token.

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import UserProfileViewSet
+from .views import UserProfileViewSet, UserLoginApiView
 
 router = DefaultRouter()
 router.register('profile', UserProfileViewSet)
 
 urlpatterns = [
+    path('login/', UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from .permissions import UpdateOwnProfile
 from .models import UserProfile
@@ -14,3 +16,7 @@ class UserProfileViewSet(ModelViewSet):
     permission_classes = (UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email')
+
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
